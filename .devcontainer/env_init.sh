798d2f20d4523f7383f83ec7aaf2ed96a1f0818d
Kinfey <93169410+kinfey@users.noreply.github.com>
2023-07-14 13:13:52 +0800

    Update env_init.sh

--- .devcontainer/env_init.sh
+++ .devcontainer/env_init.sh
@@ -5,10 +5,7 @@ docker pull qdrant/qdrant
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 
-sudo apt update
-
 sudo apt -y install nodejs
-
 
 # Install SWA CLI
 npm install -g @azure/static-web-apps-cli
