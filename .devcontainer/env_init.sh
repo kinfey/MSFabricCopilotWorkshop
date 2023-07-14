4043ab3e84a49c5cfa2c42d626e7da1fdfffd23b
Kinfey <93169410+kinfey@users.noreply.github.com>
2023-07-14 03:10:03 +0000

    main

---
+++ .devcontainer/env_init.sh
@@ -0,0 +1,14 @@
+# Install NodeJS
+nvm install v16.14.0
+
+# Install SWA CLI
+npm install -g @azure/static-web-apps-cli
+
+# Install Qdrant
+docker pull qdrant/qdrant
+
+# get Qdrant
+docker pull qdrant/qdrant
+
+# run Qdrant
+docker run -p 6333:6333 qdrant/qdrant
